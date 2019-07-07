97f1dd8dffd02ab41505cf5f1c67dcbed1fea3ff
Dowha Kim <dowha.kim@outlook.com>
2019-07-07 19:54:50 +0700

    Do function homework

--- function-homework.js
+++ function-homework.js
@@ -145,6 +145,7 @@ function distanceBetween([a, b], [c, d]) {
 
 distanceBetween([4, 5], [3, 2]);
 distanceBetween([8, 5], [3, 7]);
+5;
 
 // 15
 function middlePoint([a, b], [c, d]) {
@@ -153,35 +154,184 @@ function middlePoint([a, b], [c, d]) {
 
 middlePoint([4, 5], [3, 2]);
 middlePoint([8, 5], [3, 7]);
+
 // 16
+//왠지 다른 방법을 원하셨을 것 같은데...
+function maxAndMin(maxAndMinQ) {
+  let maxResult = Math.max.apply(null, maxAndMinQ);
+  let minResult = Math.min.apply(null, maxAndMinQ);
+  return { max: maxResult, min: minResult };
+}
 
 maxAndMin([1, 2, 5]);
 maxAndMin([-5, 2, 10, 7, 9, 50]);
 
 // 17
+function sumAll(sumAllQ){
+  let sumAllResult = 0;
+  for (let i = 0; i<sumAllQ.length; i++){
+    sumAllResult += sumAllQ[i];
+  }
+  return sumAllResult
+}
+
+sumAll([1, 2, 5]);
+sumAll([-5, 2, 10, 7, 9, 50]);
 
 // 18
+function averageAll(averageAllQ){
+  let sumForAverage = 0;
+  for (let i = 0; i<averageAllQ.length; i++){
+    sumForAverage += averageAllQ[i]
+  }
+  return sumForAverage / averageAllQ.length ;
+}
+
+averageAll([1, 2, 5]);
+averageAll([-5, 2, 10, 7, 9, 50]);
 
 // 19
+// ...
+sumHereToThere(a, b){
+
+  }
+
+sumHereToThere(1, 10);
+sumHereToThere(3, 9);
 
 // 20
+function checkIdPw(a, b){
+  let id = "manager", password = "worker2019"; 
+  if (a == id && b == password){
+    return true;
+  } else {
+    return false;
+  }
+}
+checkIdPw("manager", "worker2019")
+checkIdPw("manager", "worker2017")
 
 // 21
+// is_login not defined?
+function checkIdPw2(a, b){
+  let id = "manager", password = "worker2019", is_login = false; 
+if (a == id && b == password){
+  is_login = true;
+} else {
+  is_login = false;
+}}
+
+checkIdPw2("manager", "worker2019"); console.log(is_login);
+checkIdPw2("manager", "worker2017"); console.log(is_login); 
 
 // 22
+function addTen(addTenArr, n){
+  for(let i=0; i<addTenArr.length-(n-1); i++){
+    if (i%n == 0 ){
+    addTenArr[i+(n-1)] += 10;}
+  }
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	addTen(arr, 3);
+	console.log(arr);
 
 // 23
+function nameCheck(nameCheckObj, checkValue){
+return nameCheckObj[checkValue]
+}
+
+let obj = {name: "John", age: 13};
+nameCheck(obj, "name")
 
 // 24
+function changeName(changeNameObj, key, value){
+  return changeNameObj[key] = value;
+}
+
+let obj = {name: "John", age: 13};
+changeName(obj, "name", "Ty");
+console.log(obj.name); 
 
 // 25
+function timesTable(a){
+  let i = 1;
+  while(i<10){
+console.log(`${a} * ${i} = ${a*i}`)
+i++;
+  }
+}
+
+timesTable(3);
 
 // 26
+function divideArray(arr, a, b){
+let matrixArray = []
+for (let i = 0; i < a; i++){
+  matrixArray[i] = arr.splice(0, b)
+}
+return matrixArray
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+divideArray(arr, 3, 4)
 
 // 27
+function divideArray2(arr, a, b){
+  if(a * b == arr.length){
+  let matrixArray = []
+  for (let i = 0; i < a; i++){
+    matrixArray[i] = arr.splice(0, b)
+  }
+  return matrixArray
+  } else {
+    return null;
+  }
+}
+
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  divideArray2(arr, 3, 4)
+  divideArray2(arr, 3, 7)
 
 // 28
+function numbersCalculator(order, a, b){
+let calculator = {add: a+b, subtrack: a-b, multiply: a*b, divide:a/b, remain: a%b, power: Math.pow(a, b), absolute: Math.abs(a), log: Math.log10(a)}
+return calculator[order]
+}
+
+numbersCalculator("add", 1, 3)
+numbersCalculator("multiply", 1, 3)
+numbersCalculator("power", 4, 2)
+numbersCalculator("absolute", -5)
+numbersCalculator("log", 7)
 
 // 29
+// 이런 느낌인 것 같은데...
+function arrayCalculator(requested_array){
+let calculator = {
+  sum:  function sumAll(requested_array){
+    let sumAllResult = 0;
+    for (let i = 0; i<sumAllQ.length; i++){
+      sumAllResult += sumAllQ[i];
+    }
+    return sumAllResult
+  },
+  average: function averageAll(requested_array){
+    let sumForAverage = 0;
+    for (let i = 0; i<averageAllQ.length; i++){
+      sumForAverage += averageAllQ[i]
+    }
+    return sumForAverage / averageAllQ.length ;
+  }
+}
+  console.log(`전체 곱: ${}`)
+  console.log(`전체 합: ${calculator[sum]}`)
+  console.log(`전체 평균: ${calculator[average]}`)
+  console.log(`최댓값: ${Math.max.apply(null, requested_array)}`)
+  console.log(`최솟값: ${Math.min.apply(null, requested_array)}`)
+}
+
+arrayCalculator([5, 12, 3, 7, 9]);
 
 // 30
+// 29번 연장선...
